f747e1198f0c9a46754d34160a135d62025d5d16
Saman <teamtvrt@gmail.com>
2024-03-10 19:18:42 -0400

    Definition For Scema validator

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -1,6 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 import Joi from 'joi';
 
+/**
+ * This function will check any data in the body of the request and compare it to a given schema to ensure the data is valid.
+ * @param schema The data validation schema to check req.body against.
+ * @returns An express request handler you can insert as middleware.
+ */
 export function validate(schema: Joi.ObjectSchema) {
     return async function (req: Request, res: Response, next: NextFunction) {
         try {
